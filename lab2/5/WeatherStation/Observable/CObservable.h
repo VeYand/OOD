4b1a6ab5e510b9335ad7a12dfac281700beb25d8
VeYand <ValerikLerik17@gmail.com>
2024-10-02 23:53:09 +0300

    Инициализировал 5-е задание

---
+++ lab2/5/WeatherStation/Observable/CObservable.h
@@ -0,0 +1,60 @@
+#ifndef COBSERVABLE_H
+#define COBSERVABLE_H
+
+#include <unordered_map>
+#include <map>
+#include "IObservable.h"
+
+// Реализация интерфейса IObservable
+template<class T>
+class CObservable : public IObservable<T>
+{
+public:
+    typedef IObserver<T> ObserverType;
+
+    void RegisterObserver(unsigned priority, ObserverType &observer) override
+    {
+        m_observersByPriority.insert({priority, &observer});
+        m_observerPriorityMap[&observer] = priority;
+    }
+
+    void NotifyObservers() override
+    {
+        T data = GetChangedData();
+        auto observers = m_observersByPriority;
+        for (auto &[priority, observer]: observers)
+        {
+            observer->Update(data);
+        }
+    }
+
+    void RemoveObserver(ObserverType &observer) override
+    {
+        if (auto observerIt = m_observerPriorityMap.find(&observer); observerIt != m_observerPriorityMap.end())
+        {
+            auto priority = observerIt->second;
+            auto [first, last] = m_observersByPriority.equal_range(priority);
+
+            for (auto observerPriorityIt = first; observerPriorityIt != last; ++observerPriorityIt)
+            {
+                if (observerPriorityIt->second == &observer)
+                {
+                    m_observersByPriority.erase(observerPriorityIt);
+                    break;
+                }
+            }
+            m_observerPriorityMap.erase(observerIt);
+        }
+    }
+
+protected:
+    // Классы-наследники должны перегрузить данный метод,
+    // в котором возвращать информацию об изменениях в объекте
+    virtual T GetChangedData() const = 0;
+
+private:
+    std::multimap<unsigned, ObserverType *> m_observersByPriority;
+    std::unordered_map<ObserverType *, unsigned> m_observerPriorityMap;
+};
+
+#endif //COBSERVABLE_H
---
+++ lab2/5/WeatherStation/Observer/CStatsDisplay.h
@@ -0,0 +1,42 @@
+#ifndef CSTATSDISPLAY_H
+#define CSTATSDISPLAY_H
+
+#include <iostream>
+
+#include "CStatistics.h"
+#include "IObserver.h"
+#include "../Model/SWeatherInfo.h"
+
+class CStatsDisplay final : public IObserver<SWeatherInfo>
+{
+    /* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+    Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+    остается публичным
+    */
+    void Update(SWeatherInfo const &data) override
+    {
+        m_temperatureStatistics.AddValue(data.temperature);
+        m_humidityStatistics.AddValue(data.humidity);
+        m_pressureStatistics.AddValue(data.pressure);
+
+        std::cout << "Max Temp " << m_temperatureStatistics.GetMax() << std::endl;
+        std::cout << "Min Temp " << m_temperatureStatistics.GetMin() << std::endl;
+        std::cout << "Average Temp " << m_temperatureStatistics.GetAverage() << std::endl;
+
+        std::cout << "Max Hum " << m_humidityStatistics.GetMax() << std::endl;
+        std::cout << "Min Hum " << m_humidityStatistics.GetMin() << std::endl;
+        std::cout << "Average Hum " << m_humidityStatistics.GetAverage() << std::endl;
+
+        std::cout << "Max Pressure " << m_pressureStatistics.GetMax() << std::endl;
+        std::cout << "Min Pressure " << m_pressureStatistics.GetMin() << std::endl;
+        std::cout << "Average Pressure " << m_pressureStatistics.GetAverage() << std::endl;
+
+        std::cout << "----------------" << std::endl;
+    }
+
+    CStatistics m_temperatureStatistics{};
+    CStatistics m_humidityStatistics{};
+    CStatistics m_pressureStatistics{};
+};
+
+#endif //CSTATSDISPLAY_H
