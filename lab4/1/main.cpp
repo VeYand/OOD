7182115ad886ac617aea051474fd3b1a5f297135
VeYand <ValerikLerik17@gmail.com>
2024-10-09 23:33:46 +0300

    error: cannot convert ‘int’ to ‘const string&’

--- lab4/1/main.cpp
+++ lab4/1/main.cpp
@@ -17,7 +17,7 @@ int main()
 	Painter painter;
 	const Client client(designer);
 	client.HandleCommand(std::cin, canvas, painter);
-	canvas.SaveToFile('output.txt');
+	canvas.SaveToFile("output.txt");
 
 	return EXIT_SUCCESS;
 }
