7a607f177d466719ff182e01c4652866f9537cc5
VeYand <ValerikLerik17@gmail.com>
2024-10-28 00:47:43 +0300

    Инициализация 1-го задания 6-й лабы

---
+++ lab6/1/modern_graphics_lib.h
@@ -0,0 +1,74 @@
+#ifndef MODERN_GRAPHICS_LIB_H
+#define MODERN_GRAPHICS_LIB_H
+#include <format>
+#include <ostream>
+
+// Пространство имен современной графической библиотеки (недоступно для изменения)
+namespace modern_graphics_lib
+{
+	class CPoint
+	{
+	public:
+		CPoint(int x, int y) : x(x), y(y)
+		{
+		}
+
+		int x;
+		int y;
+	};
+
+	// Класс для современного рисования графики
+	class CModernGraphicsRenderer
+	{
+	public:
+		explicit CModernGraphicsRenderer(std::ostream &strm) : m_out(strm)
+		{
+		}
+
+		~CModernGraphicsRenderer()
+		{
+			if (m_drawing) // Завершаем рисование, если оно было начато
+			{
+				EndDraw();
+			}
+		}
+
+		// Этот метод должен быть вызван в начале рисования
+		void BeginDraw()
+		{
+			if (m_drawing)
+			{
+				throw std::logic_error("Drawing has already begun");
+			}
+			m_out << "<draw>" << std::endl;
+			m_drawing = true;
+		}
+
+		// Выполняет рисование линии
+		void DrawLine(const CPoint &start, const CPoint &end)
+		{
+			if (!m_drawing)
+			{
+				throw std::logic_error("DrawLine is allowed between BeginDraw()/EndDraw() only");
+			}
+			m_out << std::format(R"(  <line fromX="%1%" fromY="%2" toX="%3%" toY="%4%"/>)") << std::endl;
+		}
+
+		// Этот метод должен быть вызван в конце рисования
+		void EndDraw()
+		{
+			if (!m_drawing)
+			{
+				throw std::logic_error("Drawing has not been started");
+			}
+			m_out << "</draw>" << std::endl;
+			m_drawing = false;
+		}
+
+	private:
+		std::ostream &m_out;
+		bool m_drawing = false;
+	};
+}
+
+#endif //MODERN_GRAPHICS_LIB_H
---
+++ lab6/1/shape_drawing_lib.h
@@ -0,0 +1,77 @@
+#ifndef SHAPE_DRAWING_LIB_H
+#define SHAPE_DRAWING_LIB_H
+#include "graphics_lib.h"
+
+// Пространство имен библиотеки для рисования фигур (использует graphics_lib)
+// Код библиотеки недоступен для изменения
+namespace shape_drawing_lib
+{
+	struct Point
+	{
+		int x;
+		int y;
+	};
+
+	// Интерфейс объектов, которые могут быть нарисованы на холсте из graphics\_lib
+	class ICanvasDrawable
+	{
+	public:
+		virtual void Draw(graphics_lib::ICanvas &canvas) const = 0;
+
+		virtual ~ICanvasDrawable() = default;
+	};
+
+	class CTriangle : public ICanvasDrawable
+	{
+	public:
+		CTriangle(const Point &p1, const Point &p2, const Point &p3)
+		{
+			// TODO: написать код конструктора
+		}
+
+		void Draw(graphics_lib::ICanvas &canvas) const override
+		{
+			// TODO: написать код рисования треугольника на холсте
+		}
+
+	private:
+		// TODO: дописать приватную часть
+	};
+
+	class CRectangle : public ICanvasDrawable
+	{
+	public:
+		CRectangle(const Point &leftTop, int width, int height)
+		{
+			// TODO: написать код конструктора
+		}
+
+		void Draw(graphics_lib::ICanvas &canvas) const override
+		{
+			// TODO: написать код рисования прямоугольника на холсте
+		}
+
+	private:
+		// TODO: дописать приватную часть
+	};
+
+	// Художник, способный рисовать ICanvasDrawable-объекты на ICanvas
+	class CCanvasPainter
+	{
+	public:
+		CCanvasPainter(graphics_lib::ICanvas &canvas)
+		{
+			// TODO: дописать конструктор класса
+		}
+
+		void Draw(const ICanvasDrawable &drawable)
+		{
+			// TODO: дописать код рисования ICanvasDrawable на canvas, переданном в конструктор
+		}
+
+	private:
+		// TODO: дописать приватную часть
+	};
+}
+
+#endif //SHAPE_DRAWING_LIB_H
