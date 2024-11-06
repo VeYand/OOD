1681ca16945f6ae509f567ca36485626a8946e46
VeYand <ValerikLerik17@gmail.com>
2024-11-07 00:54:17 +0300

    lab7 реализовал CSlide

--- lab7/src/Slide/CSlide.h
+++ lab7/src/Slide/CSlide.h
@@ -1,32 +1,107 @@
 #ifndef CSLIDE_H
 #define CSLIDE_H
-#include <limits>
+
 #include <memory>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
 
 #include "ISlide.h"
 #include "../Shape/IShape.h"
-#include "../CommonTypes.h"
+#include "../Shape/IGroupShape.h"
+#include "../Shape/GroupShape.h"
 
 class CSlide final : public ISlide
 {
 public:
-	[[nodiscard]] double GetWidth() const override;
+	CSlide(const double width, const double height)
+		: m_width(width)
+		  , m_height(height)
+	{
+	}
 
-	[[nodiscard]] double GetHeight() const override;
+	[[nodiscard]] double GetWidth() const override
+	{
+		return m_width;
+	}
 
-	[[nodiscard]] size_t GetShapesCount() const override;
+	[[nodiscard]] double GetHeight() const override
+	{
+		return m_height;
+	}
 
-	[[nodiscard]] IShapes &GetShapes() const override;
+	[[nodiscard]] size_t GetShapesCount() const override
+	{
+		return m_shapes.size();
+	}
 
-	std::shared_ptr<IShape> GetShapeAtIndex(size_t index) override;
+	[[nodiscard]] IShapes &GetShapes() const override
+	{
+		return *const_cast<CSlide *>(this);
+	}
 
-	void RemoveShapeAtIndex(size_t index) override;
+	std::shared_ptr<IShape> GetShapeAtIndex(const size_t index) override
+	{
+		if (index >= m_shapes.size())
+		{
+			throw std::out_of_range("Index is out of range");
+		}
+		return m_shapes[index];
+	}
 
-	void GroupShapesAtIndexex(std::vector<size_t> indexes) override;
+	void RemoveShapeAtIndex(const size_t index) override
+	{
+		if (index >= m_shapes.size())
+		{
+			throw std::out_of_range("Index is out of range");
+		}
+		m_shapes.erase(m_shapes.begin() + static_cast<std::vector<int>::difference_type>(index));
+	}
 
-	void InsertShape(const std::shared_ptr<IShape> &shape, size_t position) override;
+	void GroupShapesAtIndexex(std::vector<size_t> indexes) override
+	{
+		std::vector<std::shared_ptr<IShape> > groupShapes;
+		std::ranges::sort(indexes, std::greater<>());
 
-	void Draw(ICanvas &canvas) const override;
+		for (const size_t index: indexes)
+		{
+			if (index >= m_shapes.size())
+			{
+				throw std::out_of_range("Index is out of range");
+			}
+			groupShapes.push_back(m_shapes[index]);
+			m_shapes.erase(m_shapes.begin() + static_cast<std::vector<int>::difference_type>(index));
+		}
+
+		const auto group = std::make_shared<GroupShape>();
+		for (auto &shape: groupShapes)
+		{
+			group->InsertShape(shape, group->GetShapesCount());
+		}
+		m_shapes.push_back(group);
+	}
+
+	void InsertShape(const std::shared_ptr<IShape> &shape, size_t position) override
+	{
+		if (position > m_shapes.size())
+		{
+			position = m_shapes.size();
+		}
+		m_shapes.insert(m_shapes.begin() + static_cast<std::vector<int>::difference_type>(position), shape);
+	}
+
+	void Draw(ICanvas &canvas) const override
+	{
+		for (const auto &shape: m_shapes)
+		{
+			shape->Draw(canvas);
+		}
+	}
+
+private:
+	double m_width;
+	double m_height;
+	std::vector<std::shared_ptr<IShape> > m_shapes;
 };
 
 #endif //CSLIDE_H
--- lab7/src/Slide/ISlide.h
+++ lab7/src/Slide/ISlide.h
@@ -4,24 +4,16 @@
 #include "../IDrawable.h"
 #include "../Shape/IShapes.h"
 
-class ISlide : public IDrawable
+class ISlide : public IDrawable, public IShapes
 {
 public:
 	[[nodiscard]] virtual double GetWidth() const = 0;
 
 	[[nodiscard]] virtual double GetHeight() const = 0;
 
-	[[nodiscard]] virtual size_t GetShapesCount() const = 0;
-
 	[[nodiscard]] virtual IShapes &GetShapes() const = 0;
 
-	virtual std::shared_ptr<IShape> GetShapeAtIndex(size_t index) = 0;
-
-	virtual void RemoveShapeAtIndex(size_t index) = 0;
-
 	virtual void GroupShapesAtIndexex(std::vector<size_t> indexes) = 0;
-
-	virtual void InsertShape(const std::shared_ptr<IShape> &shape, size_t position) = 0;
 
 	void Draw(ICanvas &canvas) const override = 0;
 
