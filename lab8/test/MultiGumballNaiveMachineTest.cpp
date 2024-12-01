ffe04f0db21ed19dc51b74d1c7094ecc8ea1ff39
VeYand <ValerikLerik17@gmail.com>
2024-12-01 17:59:26 +0300

    Добавил тесты для наивной реализации

--- lab8/test/MultiGumballNaiveMachineTest.cpp
+++ lab8/test/MultiGumballNaiveMachineTest.cpp
@@ -1,109 +1,215 @@
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "../src/multi_gumball_machine/naive/CMultiGumballNaiveMachine.h"
 
 using namespace multi_gumball_naive_machine;
 
-TEST (MultiGumballNaiveMachineTest, InitializationWithZero)
+std::string GetExpectedNaiveMachineStringState(unsigned count, const std::string &stateString)
 {
+	return std::format(R"(
+				Mighty Gumball, Inc.
+				C++-enabled Standing Gumball Model #2016
+				Inventory: {} gumball{}
+				Machine is {}
+			)", count, count != 1 ? "s" : "", stateString);
 }
 
-TEST (MultiGumballNaiveMachineTest, InitializationPositive)
+TEST(MultiGumballNaiveMachineTest, InitializationWithZero)
 {
+	CMultiGumballMachine machine(0);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, InsertQuarterCantInsertAnother)
+TEST(MultiGumballNaiveMachineTest, InitializationPositive)
 {
+	CMultiGumballMachine machine(3);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(3, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, InsertQuarterCantInsertOnSoldOut)
+TEST(MultiGumballNaiveMachineTest, InsertQuarterCantInsertAnother)
 {
+	CMultiGumballMachine machine(5);
+	for (int i = 0; i < 5; ++i)
+	{
+		machine.InsertQuarter();
+	}
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(5, "waiting for turn of crank"));
+	machine.InsertQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(5, "waiting for turn of crank"));
 }
 
-TEST (MultiGumballNaiveMachineTest, InsertQuarterCantInsertOnNoQuarter)
+TEST(MultiGumballNaiveMachineTest, InsertQuarterCantInsertOnSoldOut)
 {
+	CMultiGumballMachine machine(0);
+	machine.InsertQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, InsertQuarterCantInsertOnHasQuarter)
+TEST(MultiGumballNaiveMachineTest, InsertQuarterCantInsertOnNoQuarter)
 {
+	CMultiGumballMachine machine(5);
+	machine.InsertQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(5, "waiting for turn of crank"));
 }
 
-TEST (MultiGumballNaiveMachineTest, InsertQuarterCantInsertOnSold)
+TEST(MultiGumballNaiveMachineTest, InsertQuarterCantInsertOnHasQuarter)
 {
+	CMultiGumballMachine machine(5);
+	machine.InsertQuarter();
+	machine.InsertQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(5, "waiting for turn of crank"));
 }
 
-TEST (MultiGumballNaiveMachineTest, EjectQuarterOnSoldOutPosotiveQuarterCount)
+TEST(MultiGumballNaiveMachineTest, InsertQuarterCantInsertOnSold)
 {
+	CMultiGumballMachine machine(1);
+	machine.InsertQuarter();
+	machine.TurnCrank();
+	machine.InsertQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, EjectQuarterOnSoldOutZeroQuarterCount)
+TEST(MultiGumballNaiveMachineTest, EjectQuarterOnSoldOutPosotiveQuarterCount)
 {
+	CMultiGumballMachine machine(0);
+	machine.InsertQuarter();
+	machine.EjectQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, EjectQuarterOnNoQuarter)
+TEST(MultiGumballNaiveMachineTest, EjectQuarterOnSoldOutZeroQuarterCount)
 {
+	CMultiGumballMachine machine(0);
+	machine.EjectQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, EjectQuarterOnHasQuarter)
+TEST(MultiGumballNaiveMachineTest, EjectQuarterOnNoQuarter)
 {
+	CMultiGumballMachine machine(5);
+	machine.EjectQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(5, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, EjectQuarterOnSold)
+TEST(MultiGumballNaiveMachineTest, EjectQuarterOnHasQuarter)
 {
+	CMultiGumballMachine machine(5);
+	machine.InsertQuarter();
+	machine.EjectQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(5, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, TurnCrankOnSoldOut)
+TEST(MultiGumballNaiveMachineTest, EjectQuarterOnSold)
 {
+	CMultiGumballMachine machine(1);
+	machine.InsertQuarter();
+	machine.TurnCrank();
+	machine.EjectQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, TurnCrankOnNoQuarter)
+TEST(MultiGumballNaiveMachineTest, TurnCrankOnSoldOut)
 {
+	CMultiGumballMachine machine(0);
+	machine.TurnCrank();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, TurnCrankOnHasQuarter)
+TEST(MultiGumballNaiveMachineTest, TurnCrankOnNoQuarter)
 {
+	CMultiGumballMachine machine(5);
+	machine.TurnCrank();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(5, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, TurnCrankOnSold)
+TEST(MultiGumballNaiveMachineTest, TurnCrankOnHasQuarter)
 {
+	CMultiGumballMachine machine(5);
+	machine.InsertQuarter();
+	machine.TurnCrank();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(4, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, RefillOnSoldOutZeroBallCount)
+TEST(MultiGumballNaiveMachineTest, TurnCrankOnSold)
 {
+	CMultiGumballMachine machine(1);
+	machine.InsertQuarter();
+	machine.TurnCrank();
+	machine.TurnCrank();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, RefillOnSoldOutNonZeroBallCountZeroQuarterCount)
+TEST(MultiGumballNaiveMachineTest, RefillOnSoldOutZeroBallCount)
 {
+	CMultiGumballMachine machine(0);
+	machine.Refill(0);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, RefillOnSoldOutNonZeroBallCountNonZeroQuarterCount)
+TEST(MultiGumballNaiveMachineTest, RefillOnSoldOutNonZeroBallCountZeroQuarterCount)
 {
+	CMultiGumballMachine machine(0);
+	machine.Refill(3);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(3, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, RefillOnNoQuarter)
+TEST(MultiGumballNaiveMachineTest, RefillOnSoldOutNonZeroBallCountNonZeroQuarterCount)
 {
+	CMultiGumballMachine machine(0);
+	testing::internal::CaptureStdout();
+	machine.InsertQuarter();
+	const std::string output = testing::internal::GetCapturedStdout();
+	EXPECT_EQ(output, "You can't insert a quarter, the machine is sold out\n");
+	machine.Refill(3);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(3, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, RefillOnHasQuarter)
+TEST(MultiGumballNaiveMachineTest, RefillOnNoQuarter)
 {
+	CMultiGumballMachine machine(5);
+	machine.Refill(10);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(10, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, RefillOnSold)
+TEST(MultiGumballNaiveMachineTest, RefillOnHasQuarter)
 {
+	CMultiGumballMachine machine(5);
+	machine.InsertQuarter();
+	machine.Refill(10);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(10, "waiting for turn of crank"));
 }
 
-TEST (MultiGumballNaiveMachineTest, ToStringOnSoldOut)
+TEST(MultiGumballNaiveMachineTest, RefillOnSold)
 {
+	CMultiGumballMachine machine(1);
+	machine.InsertQuarter();
+	machine.TurnCrank();
+	machine.Refill(10);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(10, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, ToStringOnNoQuarter)
+TEST(MultiGumballNaiveMachineTest, ToStringOnSoldOut)
 {
+	CMultiGumballMachine machine(0);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
 
-TEST (MultiGumballNaiveMachineTest, ToStringOnHasQuarter)
+TEST(MultiGumballNaiveMachineTest, ToStringOnNoQuarter)
 {
+	CMultiGumballMachine machine(5);
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(5, "waiting for quarter"));
 }
 
-TEST (MultiGumballNaiveMachineTest, ToStringOnSold)
+TEST(MultiGumballNaiveMachineTest, ToStringOnHasQuarter)
 {
+	CMultiGumballMachine machine(5);
+	machine.InsertQuarter();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(5, "waiting for turn of crank"));
+}
+
+TEST(MultiGumballNaiveMachineTest, ToStringOnSold)
+{
+	CMultiGumballMachine machine(1);
+	machine.InsertQuarter();
+	machine.TurnCrank();
+	EXPECT_EQ(machine.ToString(), GetExpectedNaiveMachineStringState(0, "sold out"));
 }
